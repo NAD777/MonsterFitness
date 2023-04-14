5a5132b6472833b00beff0e2a050468eb80ca084
Denis <fpv.airplane.barnaul@gmail.com>
2023-04-14 14:48:06 +0300

    add get method for retrieving stepcount for previous year

--- MonsterFitness/Service/StepCountModel.swift
+++ MonsterFitness/Service/StepCountModel.swift
@@ -15,7 +15,7 @@ final class StepCountModel: Error {
 
     private let healthStore = HKHealthStore()
 
-    public func readStepCount(completion: @escaping (Double) -> Void) throws {
+    public func getStepCountForToday(completion: @escaping (Double) -> Void) throws {
         guard let stepQuantityType = HKQuantityType.quantityType(forIdentifier: .stepCount) else {
             throw Errors.unknownError
         }
@@ -34,6 +34,37 @@ final class StepCountModel: Error {
             completion(sum.doubleValue(for: HKUnit.count()))
         }
         healthStore.execute(query)
-
     }
+
+    // получаем данные о шагах за прошлый год
+    public func getStepCountsForPreviousYear(completion: @escaping ([Date: Int]) -> Void ) {
+        let healthStore = HKHealthStore()
+        let stepCountQuantity = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+        let calendar = Calendar.current
+
+        let endDate = Date()
+        let startDate = calendar.date(byAdding: .day, value: -365, to: endDate)!
+
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: .strictStartDate)
+        let query = HKStatisticsCollectionQuery(quantityType: stepCountQuantity, quantitySamplePredicate: predicate,
+                    options: .cumulativeSum, anchorDate: startDate, intervalComponents: DateComponents(day: 1))
+
+        var stepCounts: [Date: Int] = [:]
+        query.initialResultsHandler = { _, results, error in
+            guard let results = results else {
+                print("Failed to retrieve step counts: \(error?.localizedDescription ?? "Unknown error")")
+                return
+            }
+
+            // Iterate through each day's step count data and add it to the dictionary
+            results.enumerateStatistics(from: startDate, to: endDate) { statistics, _ in
+                let date = statistics.startDate
+                let stepCount = Int(statistics.sumQuantity()?.doubleValue(for: .count()) ?? 0)
+                stepCounts[date] = stepCount
+            }
+            completion(stepCounts)
+        }
+        healthStore.execute(query)
+    }
+
 }
