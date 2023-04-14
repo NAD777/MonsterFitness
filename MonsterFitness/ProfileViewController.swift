6b76dc71a0ec43de46211fd41034e41ef577194c
Dmitry <dmitry@MacBook-Pro-Delariu.local>
2023-04-14 16:10:20 +0300

    Create ProfileViewController

---
+++ MonsterFitness/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  MonsterFitness
+//
+//  Created by Dmitry on 14.04.2023.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the viewa
+        view.backgroundColor = .blue
+    }
+}
