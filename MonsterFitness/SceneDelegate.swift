7727b1844a0504679fec22cc661f9c88f25ef057
Denis <fpv.airplane.barnaul@gmail.com>
2023-04-15 23:29:47 +0300

    Main screen layout+mockdata

--- MonsterFitness/SceneDelegate.swift
+++ MonsterFitness/SceneDelegate.swift
@@ -16,9 +16,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             guard let windowScene = (scene as? UIWindowScene) else { return }
 
             let window = UIWindow(windowScene: windowScene)
-            let vc = RootViewController()
+            let vc = MainScreen()
+//            let vc = RootViewController()
         //  application.windows.first?.rootViewController = vc
-            router = Router(rootViewController: vc)
+//            router = Router(rootViewController: vc)
             window.rootViewController = vc
             self.window = window
             window.makeKeyAndVisible()
