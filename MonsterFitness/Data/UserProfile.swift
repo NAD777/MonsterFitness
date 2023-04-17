9b658d7da7fc27fe51964a3a23eb2b6d4b479dc7
Dmitry <dmitry@MacBook-Pro-Delariu.local>
2023-04-17 14:47:03 +0300

    Start create user default

---
+++ MonsterFitness/Data/UserProfile.swift
@@ -0,0 +1,27 @@
+//
+//  UserProfile.swift
+//  MonsterFitness
+//
+//  Created by Dmitry on 17.04.2023.
+//
+
+import Foundation
+
+final class UserProfile {
+    var currentUser: User? {
+        didSet {
+            
+        }
+    }
+    init() {
+        let defaults = UserDefaults.standard
+        let name = defaults.string(forKey: "name") ?? "Unknow"
+        let age = defaults.integer(forKey: "age")
+        let weight = defaults.integer(forKey: "weight")
+        let height = defaults.integer(forKey: "height")
+        let target = defaults.integer(forKey: "target")
+        let genders = ["male": Genders.male, "female": Genders.female, "other": Genders.other]
+        let currentGender = genders[defaults.string(forKey: "gender") ?? "other"]
+        currentUser = User(name: name, age: age, weight: weight, height: height, gender: currentGender, target: target)
+    }
+}
