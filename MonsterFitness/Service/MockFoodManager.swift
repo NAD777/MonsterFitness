0534aede15966da55dbf1dd097e5eb53803224c2
Vitaly Korziuk <korziuk@yandex-team.ru>
2023-04-17 15:49:19 +0300

    Fixed duplicate Dish struct

--- MonsterFitness/Service/MockFoodManager.swift
+++ MonsterFitness/Service/MockFoodManager.swift
@@ -7,21 +7,21 @@
 
 import Foundation
 
-enum DayPart: String {
-    case breakfast = "Завтрак"
-    case lunch = "Обед"
-    case dinner = "Ужин"
-    case unspecified = "Не указан"
-}
-
-struct Dish {
-    let title: String
-    let calories: Double
-    let carbs: Double
-    let fat: Double
-}
-
 struct Portion {
+    enum DayPart: String {
+        case breakfast = "Завтрак"
+        case lunch = "Обед"
+        case dinner = "Ужин"
+        case unspecified = "Не указан"
+    }
+
+    struct Dish {
+        let title: String
+        let calories: Double
+        let carbs: Double
+        let fat: Double
+    }
+
     let weightConsumed: Double
     let dishConsumed: Dish
     let dayPart: DayPart
@@ -29,11 +29,11 @@ struct Portion {
 
 class MockFoodManager {
     public var storage: [Portion] = [
-        Portion(weightConsumed: 0.500, dishConsumed: Dish(title: "Пиво", calories: 30, carbs: 9, fat: 0), dayPart: .dinner),
-        Portion(weightConsumed: 0.400, dishConsumed: Dish(title: "Шницель", calories: 30, carbs: 9, fat: 0), dayPart: .lunch),
-        Portion(weightConsumed: 0.500, dishConsumed: Dish(title: "Суп", calories: 30, carbs: 9, fat: 0), dayPart: .unspecified),
-        Portion(weightConsumed: 0.500, dishConsumed: Dish(title: "Рамен", calories: 30, carbs: 9, fat: 0), dayPart: .breakfast),
-        Portion(weightConsumed: 0.500, dishConsumed: Dish(title: "Пиво", calories: 30, carbs: 9, fat: 0), dayPart: .dinner)
+        Portion(weightConsumed: 0.500, dishConsumed: Portion.Dish(title: "Пиво", calories: 30, carbs: 9, fat: 0), dayPart: .dinner),
+        Portion(weightConsumed: 0.400, dishConsumed: Portion.Dish(title: "Шницель", calories: 30, carbs: 9, fat: 0), dayPart: .lunch),
+        Portion(weightConsumed: 0.500, dishConsumed: Portion.Dish(title: "Суп", calories: 30, carbs: 9, fat: 0), dayPart: .unspecified),
+        Portion(weightConsumed: 0.500, dishConsumed: Portion.Dish(title: "Рамен", calories: 30, carbs: 9, fat: 0), dayPart: .breakfast),
+        Portion(weightConsumed: 0.500, dishConsumed: Portion.Dish(title: "Пиво", calories: 30, carbs: 9, fat: 0), dayPart: .dinner)
     ]
     
     init(storage: [Portion]) {
