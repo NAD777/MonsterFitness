6e82ba54aa9d7b7aa851308954305e1c64aadbe1
Delariu Dmitry <107302386+vordex-dd@users.noreply.github.com>
2023-04-17 13:43:07 +0300

    Revert "Main screen layout+mockdata"

--- MonsterFitness/SceneDelegate.swift
+++ MonsterFitness/SceneDelegate.swift
@@ -16,10 +16,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             guard let windowScene = (scene as? UIWindowScene) else { return }
 
             let window = UIWindow(windowScene: windowScene)
-            let vc = MainScreen()
-//            let vc = RootViewController()
+            let vc = RootViewController()
         //  application.windows.first?.rootViewController = vc
-//            router = Router(rootViewController: vc)
+            router = Router(rootViewController: vc)
             window.rootViewController = vc
             self.window = window
             window.makeKeyAndVisible()
