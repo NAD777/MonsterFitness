bd7a321254031657d754ded4d8e1be413eb2407d
Denis <fpv.airplane.barnaul@gmail.com>
2023-04-20 09:50:27 +0300

    add method for calculating calorie expenditure for given stepcount

--- MonsterFitness/Service/ConsumptionEstimation.swift
+++ MonsterFitness/Service/ConsumptionEstimation.swift
@@ -44,24 +44,24 @@ final class ConsumptionEstimation: CalorieEstimator {
     internal func calculateBasalConsumption(user: User) -> Double {
         guard let age = user.age else {
             return 0
-//            throw Errors.ageNotSpecified
+            //            throw Errors.ageNotSpecified
         }
         guard let gender = user.gender else {
             return 0
-//            throw Errors.genderNotSpecified
+            //            throw Errors.genderNotSpecified
         }
         guard let weight = user.weight else {
             return 0
-//            throw Errors.weightNotSpecified
+            //            throw Errors.weightNotSpecified
         }
         guard let height = user.height else {
             return 0
-//            throw Errors.heightNotSpecified
+            //            throw Errors.heightNotSpecified
         }
         guard let activityLevel = user.activityLevel else {
             return 0
-//            throw Errors.activityLevelNotSpecified
-         }
+            //            throw Errors.activityLevelNotSpecified
+        }
         let activityModifier = applyActivityModifier(user: user)
         
         switch gender {
@@ -74,6 +74,32 @@ final class ConsumptionEstimation: CalorieEstimator {
         case .other:
             let otherCalories: Double = 66.5 + 11.75 * Double(weight) + 3 * Double(height) - 5.5 * Double(age) * activityModifier
             return otherCalories
+        }
+    }
+    
+    public func getCalorieExpandatureForGivenSteps(user: User, steps: Int, closure: @escaping (Double)->Void) throws {
+        guard let age = user.age else {
+            throw Errors.ageNotSpecified
+        }
+        guard let gender = user.gender else {
+            throw Errors.genderNotSpecified
+        }
+        guard let weight = user.weight else {
+            throw Errors.weightNotSpecified
+        }
+        guard let height = user.height else {
+            throw Errors.heightNotSpecified
+        }
+        guard let activityLevel = user.activityLevel else {
+            throw Errors.activityLevelNotSpecified
+        }
+        
+        let newUser = User(name: user.name, age: age, weight: weight, height: height, gender: gender,
+                           activityLevel: activityLevel)
+        
+        let basalExpandature = calculateBasalConsumption(user: newUser)
+        let physicalActivityExpandature = calculatePhysicalCalorieExpandatureForGivenSteps(user: newUser, steps: steps) { arg in
+            closure(arg+basalExpandature)
         }
     }
     
@@ -100,6 +126,18 @@ final class ConsumptionEstimation: CalorieEstimator {
         let basalExpandature = calculateBasalConsumption(user: newUser)
         let physicalActivityExpandature = calculatePhysicalCalorieExpandature(user: newUser) { arg in
             closure(arg+basalExpandature)
+        }
+    }
+    
+    
+    internal func calculatePhysicalCalorieExpandatureForGivenSteps(user: User, steps: Int, closure: @escaping (Double)->Void) {
+        // FIXME: формула сомнительная, я в математике не силен
+        Task {
+            let steps = steps
+            let caloriesSpentForAMile = 0.57 * Double(user.weight ?? 100000) * 0.453592
+            let stepLengthInMeters = Double(user.height ?? 0)/400 + 0.37
+            let caloriesSpent = Double(steps) * stepLengthInMeters / caloriesSpentForAMile
+            closure(caloriesSpent)
         }
     }
     
