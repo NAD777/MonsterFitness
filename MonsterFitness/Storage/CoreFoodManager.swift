357f73e638e769e50ed3fae7b19b859d427214ad
Yandex <yandex@MacBook-Pro-Yandex-2.local>
2023-04-21 03:01:17 +0300

    revert of revert

--- MonsterFitness/Storage/CoreFoodManager.swift
+++ MonsterFitness/Storage/CoreFoodManager.swift
@@ -14,84 +14,16 @@ final class CoreFoodManager: FoodStorage {
 
     init(date: Date, context: NSManagedObjectContext) {
         self.date = date
-        let request = NSFetchRequest<NSFetchRequestResult>(entityName: String(describing: CoreMenu.self))
-        request.predicate = NSPredicate(format: "date == %@", date as NSDate)
-        allPortions = []
-        do {
-            guard let results = try context.fetch(request) as? [CoreMenu] else {
-                return
-            }
-            if let portions = results.first?.breakfast?.array as? [CorePortion] {
-                allPortions += portions.map {
-                    Portion(
-                        weightConsumed: $0.weight,
-                        dishConsumed: Dish(
-                            title: $0.dish?.name ?? "",
-                            kcal: $0.dish?.energyKcal ?? 0,
-                            prot: $0.dish?.proteinG ?? 0,
-                            fat: $0.dish?.fatG ?? 0,
-                            carb: $0.dish?.carbsG ?? 0
-                        ),
-                        dayPart: .breakfast)
-                }
-            }
-            if let portions = results.first?.lunch?.array as? [CorePortion] {
-                allPortions += portions.map {
-                    Portion(
-                        weightConsumed: $0.weight,
-                        dishConsumed: Dish(
-                            title: $0.dish?.name ?? "",
-                            kcal: $0.dish?.energyKcal ?? 0,
-                            prot: $0.dish?.proteinG ?? 0,
-                            fat: $0.dish?.fatG ?? 0,
-                            carb: $0.dish?.carbsG ?? 0
-                        ),
-                        dayPart: .lunch)
-                }
-            }
-            if let portions = results.first?.dinner?.array as? [CorePortion] {
-                allPortions += portions.map {
-                    Portion(
-                        weightConsumed: $0.weight,
-                        dishConsumed: Dish(
-                            title: $0.dish?.name ?? "",
-                            kcal: $0.dish?.energyKcal ?? 0,
-                            prot: $0.dish?.proteinG ?? 0,
-                            fat: $0.dish?.fatG ?? 0,
-                            carb: $0.dish?.carbsG ?? 0
-                        ),
-                        dayPart: .dinner)
-                }
-            }
-            if let portions = results.first?.other?.array as? [CorePortion] {
-                allPortions += portions.map {
-                    Portion(
-                        weightConsumed: $0.weight,
-                        dishConsumed: Dish(
-                            title: $0.dish?.name ?? "",
-                            kcal: $0.dish?.energyKcal ?? 0,
-                            prot: $0.dish?.proteinG ?? 0,
-                            fat: $0.dish?.fatG ?? 0,
-                            carb: $0.dish?.carbsG ?? 0
-                        ),
-                        dayPart: .unspecified)
-                }
-            }
-        } catch {
-            allPortions = []
-        }
+        updateStorage()
     }
      
     public func updateStorage() {
-        let date = Calendar.current.startOfDay(for: Date())
         let context = CoreStorage().persistentContainer.viewContext
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: String(describing: CoreMenu.self))
         request.predicate = NSPredicate(format: "date == %@", date as NSDate)
 
         do {
-            guard let results = try context.fetch(request) as? [CoreMenu],
-                let portions = results.first?.breakfast?.array as? [CorePortion] else {
-                allPortions = []
+            guard let results = try context.fetch(request) as? [CoreMenu] else {
                 return
             }
             allPortions = []
